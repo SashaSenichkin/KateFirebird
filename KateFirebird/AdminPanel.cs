5155e1922f645ba35ea6dfdbf8d849466aeaead1
SashaSenichkin <30441534+SashaSenichkin@users.noreply.github.com>
2021-07-09 17:17:56 +0300

    admin panel initial

---
+++ KateFirebird/AdminPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KateFirebird
+{
+    public partial class AdminPanel : Form
+    {
+        public AdminPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
