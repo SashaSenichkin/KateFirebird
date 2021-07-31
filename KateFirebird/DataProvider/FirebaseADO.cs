878c9187729dfefb09f353850ebfbb37faf5d9f1
30441534+SashaSenichkin@users.noreply.github.com <30441534+SashaSenichkin@users.noreply.github.com>
2021-07-31 14:36:43 +0300

    ado provider tested

--- KateFirebird/DataProvider/FirebaseADO.cs
+++ KateFirebird/DataProvider/FirebaseADO.cs
@@ -3,13 +3,13 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
-using System.Data.OleDb;
+using FirebirdSql.Data.FirebirdClient;
 
 namespace KateFirebird
 {
     public class FirebaseADO : IDataProvider
     {
-        OleDbConnection Connection;
+        FbConnection Connection;
         public List<Breed> Breeds { get; set; }
         public List<Cell> Cells { get; set; }
         public List<Chicken> Chickens { get; set; }
@@ -17,41 +17,75 @@ namespace KateFirebird
 
         public FirebaseADO()
         {
-            OleDbConnectionStringBuilder cb = new OleDbConnectionStringBuilder();
-            cb.Provider = "LCPI.IBProvider";
-            cb.Add("Location", @"localhost: d:\Program Files\Firebird\examples\CHICKENFARM.FDB");
-            cb.Add("User ID", "sysdba");
-            cb.Add("Password", "masterkey");
-            cb.Add("ctype", "win1251");
+            var cb = new StringBuilder();
+            cb.Append("User=SYSDBA;");
+            cb.Append("Password=111;");
+            cb.Append("DataSource=localhost;");
+            cb.Append("Database=KATEFIREBIRD.fdb;");
 
-            Connection = new OleDbConnection(cb.ToString());
+            var t = new FbConnectionStringBuilder();
+            t.Database = @"E:\Git\KateFirebird\Db\KATEFIREBIRD.FDB";
+            t.DataSource = "localhost";
+            t.Password = "111";
+            t.UserID = "SYSDBA";
+            t.Charset = "NONE";
+            t.WireCrypt = FbWireCrypt.Enabled;
+  
+            Connection = new FbConnection(t.ToString());
             UpdateTables();
         }
 
         private void UpdateTables()
         {
+            Workers = GetAllObjectsOfType<Worker>();
+            Breeds = GetAllObjectsOfType<Breed>();
+            Cells = GetAllObjectsOfType<Cell>();
+            Chickens = GetAllObjectsOfType<Chicken>();
+        }
+
+        private List<T> GetAllObjectsOfType<T>() where T : new()
+        {
             Connection.Open();
-            var trans = Connection.BeginTransaction();
-            var cmdChick = new OleDbCommand("select Id, Weight, Age, BreedId, EggCount, CellId from Chickens", Connection, trans);
-            Chickens = cmdChick.ExecuteReader().Cast<Chicken>().ToList();
+            var fields = String.Join(", ",GetFieldsNames<T>());
+            var tableAttr = typeof(T).GetCustomAttributes(false).ToList().FirstOrDefault(x => x is TableNameAttribute);
+            if (!(tableAttr is TableNameAttribute tableName))
+                throw new Exception($"add TableNameAttribute to entity {typeof(T).Name}");
 
-            var cmdCells = new OleDbCommand("select Id, DepartmentNum, RowNum, CellNum, WorkerId from Cells", Connection, trans);
-            Cells = cmdCells.ExecuteReader().Cast<Cell>().ToList();
+            var dbData = new FbCommand($"select {fields} from {tableName.TableName}", Connection);
+            var reader = dbData.ExecuteReader();
+            var values = new object[reader.FieldCount];
+            var result = new List<T>();
 
-            var cmdWorkers = new OleDbCommand("select Id, FullName, Salary from Workers", Connection, trans);
-            Workers = cmdWorkers.ExecuteReader().Cast<Worker>().ToList();
-
-            var cmdBreeds = new OleDbCommand("select Id, Name, AvgProduction, AvgWeight, DietId from Breeds", Connection, trans);
-            Breeds = cmdBreeds.ExecuteReader().Cast<Breed>().ToList();
+            while (reader.Read())
+            {
+                reader.GetValues(values);
+                result.Add(CastFromObjArray<T>(values));
+            }
 
             Connection.Close();
+            return result;
+        }
+
+        private string[] GetFieldsNames<T>() 
+        {
+            return typeof(T).GetFields().Select(x => x.Name).ToArray();
+        }
+
+        private T CastFromObjArray<T>(object[] values) where T: new()
+        {
+            var instance = new T();
+            var fields = typeof(T).GetFields();
+            for (int i = 0; i < values.Length; i++)
+                fields[i].SetValue(instance, values[i]);
+
+            return instance;
         }
 
         public void AddChicken(Chicken chicken)
         {
             Connection.Open();
             var trans = Connection.BeginTransaction();
-            var cmdChick = new OleDbCommand($"insert into Chickens values({chicken.Id}, {chicken.Weight}, {chicken.Age}, {chicken.BreedId}, 0, {chicken.CellId})", Connection, trans);
+            var cmdChick = new FbCommand($"insert into Chickens values({chicken.Id}, {chicken.Weight}, {chicken.Age}, {chicken.BreedId}, 0, {chicken.CellId})", Connection, trans);
             cmdChick.ExecuteNonQuery();
             Connection.Close();
             UpdateTables();
@@ -61,7 +95,7 @@ namespace KateFirebird
         {
             Connection.Open();
             var trans = Connection.BeginTransaction();
-            var cmdChick = new OleDbCommand($"Delete from workers where Id = {id})", Connection, trans);
+            var cmdChick = new FbCommand($"Delete from workers where Id = {id})", Connection, trans);
             cmdChick.ExecuteNonQuery();
             Connection.Close();
             UpdateTables();
@@ -71,10 +105,18 @@ namespace KateFirebird
         {
             Connection.Open();
             var trans = Connection.BeginTransaction();
-            var cmdChick = new OleDbCommand($"update Chickens set Weight = {newWeight} where id = {id})", Connection, trans);
+            var cmdChick = new FbCommand($"update Chickens set Weight = {newWeight} where id = {id})", Connection, trans);
             cmdChick.ExecuteNonQuery();
             Connection.Close();
             UpdateTables();
+        }
+    }
+    public class TableNameAttribute : Attribute
+    {
+        public string TableName;
+        public TableNameAttribute(string tableName)
+        {
+            TableName = tableName;
         }
     }
 }
